155dcdc2460007fbf438d4f42f2028d319dfa74e
Jashuwa <93178797+jamesrick123@users.noreply.github.com>
2021-12-15 11:44:21 +0530

    Add files via upload

---
+++ switch c.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+void main()
+{
+	
+	int n;
+	printf("Enter your month");
+	scanf("%d",&n);
+	switch(n)
+	{
+		 case 1:
+		 	printf("january");
+		 	break;
+		case 2:
+		 	printf("february");
+		 	break;
+		case 3:
+		 	printf("march");
+			 break;
+		case 4:
+		 	printf("april");
+			 break;
+		case 5:
+		 	printf("may");
+			 break;	 
+		case 6:
+		 	printf("june");
+			 break;	 
+		case 7:
+		 	printf("july");
+			 break;	 	 
+		case 8:
+		 	printf("auguest");
+			 break;	 
+		case 9:
+		 	printf("september");
+			 break;	 
+		case 10:
+		 	printf("october");
+			 break;	 
+		case 11:
+		 	printf("november");
+			 break;	 
+		case 12:
+		 	printf("december");
+			 break;	
+	    	default:
+		printf("invalid input"); 
+			 
+			 
+			 
+	}
+	
+	
+	
+}
